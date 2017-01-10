cb77e450e844b66b37877a55a208849584470f89
Edrenn <edern.rodriguez@orange.fr>
2017-01-10 16:02:22 +0100

    Création classe User + ajout getters setters

---
+++ src/core/Entities/User.java
@@ -0,0 +1,34 @@
+package core.Entities;
+
+public class User {
+	String nom;
+	String prenom;
+	String login;
+	String motDePasse;
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+
+	
+}
