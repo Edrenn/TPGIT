4cbc6a5aa80bf34a0feb0d2c9f2a97a02c903001
Nicolas <nicolas.lepiez@imie-rennes.fr>
2017-02-07 16:50:50 +0100

    Remove user creation from main

--- src/core/Main.java
+++ src/core/Main.java
@@ -9,22 +9,6 @@ public class Main {
 
 	public static void main (String arg[]) {
 		
-		
-		Droits write = new Droits(1, 1, "write", "Orange");
-		Droits read = new Droits(1, 0, "read", "Orange");
-		Droits move = new Droits(1, 1, "move", "Orange");
-		
-		ArrayList<Droits> totoDroit = new ArrayList();
-		totoDroit.add(write);
-		totoDroit.add(read);
-		
-		ArrayList<Droits> tataDroit = new ArrayList();
-		tataDroit.add(read);
-		tataDroit.add(move);
-		
-		User toto = new User("Toto", "Jean", "totoJ", "jeanT", totoDroit);
-		User tata = new User("Tata", "Camille", "tataC", "CamilleT", tataDroit);
-		
 	}
 
 
