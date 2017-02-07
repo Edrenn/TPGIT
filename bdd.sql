9e96e87d0fcfbef55329032ef1183bbe7ca7426c
Nicolas <nicolas.lepiez@imie-rennes.fr>
2017-02-07 16:51:13 +0100

    Add sql file to create and insert in db

---
+++ bdd.sql
@@ -0,0 +1,73 @@
+#------------------------------------------------------------
+#        Script MySQL.
+#------------------------------------------------------------
+
+
+#------------------------------------------------------------
+# Create DATABASE and USER
+#------------------------------------------------------------
+CREATE DATABASE TPGit;
+CREATE USER "tpgit"@"localhost" IDENTIFIED BY "pwd";
+
+#------------------------------------------------------------
+# Table: User
+#------------------------------------------------------------
+
+CREATE TABLE User(
+        user_id     int (11) Auto_increment  NOT NULL ,
+        user_nom    Varchar (25) ,
+        user_prenom Varchar (25) ,
+        user_login  Varchar (25) ,
+        user_mdp    Varchar (25) ,
+        PRIMARY KEY (user_id )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Droits
+#------------------------------------------------------------
+
+CREATE TABLE Droits(
+        droits_id       int (11) Auto_increment  NOT NULL ,
+        droits_niv      Int ,
+        droits_severite Int ,
+        droits_nom      Varchar (25) ,
+        droits_societe  Varchar (25) ,
+        PRIMARY KEY (droits_id )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Possede
+#------------------------------------------------------------
+
+CREATE TABLE Possede(
+        user_id   Int NOT NULL ,
+        droits_id Int NOT NULL ,
+        PRIMARY KEY (user_id ,droits_id )
+)ENGINE=InnoDB;
+
+ALTER TABLE Possede ADD CONSTRAINT FK_Possede_user_id FOREIGN KEY (user_id) REFERENCES User(user_id);
+ALTER TABLE Possede ADD CONSTRAINT FK_Possede_droits_id FOREIGN KEY (droits_id) REFERENCES Droits(droits_id);
+
+#------------------------------------------------------------
+# USER RIGTHS
+#------------------------------------------------------------
+
+GRANT ALL ON TPGit.* TO "tpgit"@"localhost" IDENTIFIED BY "pwd";
+
+#------------------------------------------------------------
+# INSERT
+#------------------------------------------------------------
+
+INSERT INTO Droits VALUES(NULL, 1, 1, "write", "Orange");
+INSERT INTO Droits VALUES(NULL, 1, 0, "read", "Orange");
+INSERT INTO Droits VALUES(NULL, 1, 2, "move", "Orange");
+
+INSERT INTO User VALUES(NULL, "Toto", "Jean", "totoJ", "JeanT");
+INSERT INTO Possede VALUES(1, 1);
+INSERT INTO Possede VALUES(1, 2);
+
+INSERT INTO User VALUES(NULL, "Tata", "Camille", "tataC", "CamilleT");
+INSERT INTO Possede VALUES(2, 2);
+INSERT INTO Possede VALUES(2, 3);
